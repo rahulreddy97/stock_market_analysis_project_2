833f3be3615cb0a4cf0c2a256fa826bb74624255
Rahul reddy <yallarahulreddy2013@gmail.com>
2021-03-16 00:38:55 -0500

    Update stock_predector2.py

--- stock_predector2.py
+++ stock_predector2.py
@@ -5,22 +5,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 finviz_url = 'https://finviz.com/quote.ashx?t='
-tickers = ['AMZN', 'GOOG', 'FB']
+stocks = ['AMZN', 'GOOG', 'FB']
 
 news_tables = {}
-for ticker in tickers:
-    url = finviz_url + ticker
+for stock in stocks:
+    url = finviz_url + stock
 
     req = Request(url=url, headers={'user-agent': 'my-app'})
     response = urlopen(req)
 
     html = BeautifulSoup(response, features='html.parser')
     news_table = html.find(id='news-table')
-    news_tables[ticker] = news_table
+    news_tables[stock] = news_table
 
 parsed_data = []
 
-for ticker, news_table in news_tables.items():
+for stock, news_table in news_tables.items():
 
     for row in news_table.findAll('tr'):
 
@@ -33,9 +33,9 @@ for ticker, news_table in news_tables.items():
             date = date_data[0]
             time = date_data[1]
 
-        parsed_data.append([ticker, date, time, title])
+        parsed_data.append([stock, date, time, title])
 
-df = pd.DataFrame(parsed_data, columns=['ticker', 'date', 'time', 'title'])
+df = pd.DataFrame(parsed_data, columns=['stock', 'date', 'time', 'title'])
 
 vader = SentimentIntensityAnalyzer()
 
@@ -44,7 +44,30 @@ df['compound'] = df['title'].apply(f)
 df['date'] = pd.to_datetime(df.date).dt.date
 
 plt.figure(figsize=(10,8))
-mean_df = df.groupby(['ticker', 'date']).mean().unstack()
+mean_df = df.groupby(['stock', 'date']).mean().unstack()
 mean_df = mean_df.xs('compound', axis="columns")
 mean_df.plot(kind='bar')
+plt.show()
+
+plt.figure(figsize=(12,8))
+mean_df = df.groupby(['stock', 'date']).mean().unstack()
+mean_df = mean_df.xs('compound', axis="columns")
+plt.style.use("ggplot")
+
+amz = mean_df.loc["AMZN"]
+fb = mean_df.loc["FB"]
+goog = mean_df.loc["GOOG"]
+amz.drop(amz[amz.isna()==True].index, inplace=True)
+xtickspos = []
+for i in range(len(amz.values)):
+    xtickspos.append(i+2.5)
+for i in range(len(fb.values)):
+    xtickspos.append(i+9)
+for i in range(len(goog.values)):
+    xtickspos.append(i+19.5)
+plt.bar(xtickspos[0: len(amz.values)] , amz.values)
+plt.bar(xtickspos[len(amz.values): len(fb.values)+len(amz.values)] , fb.values)
+plt.bar(xtickspos[len(fb.values)+len(amz.values):] , goog.values)
+plt.xticks(xtickspos, np.concatenate((amz.index, fb.index, goog.index)), rotation=90)
+plt.legend(["AMZN", "FB", "GOOG"])
 plt.show()
